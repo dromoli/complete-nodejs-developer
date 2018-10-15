a0ebdeb54b1b25e09fa3b686b1aec15adceb1862
Diego Romoli <diego.romoli@intelligentpathways.com.au>
2018-10-15 15:10:17 +1000

    Update

--- notes/app.js
+++ notes/app.js
@@ -1,21 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-var user = os.userInfo();
-
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function(err) {
-    if(err) {
-        console.log('Unable to write to the file');
-    } else {
-        console.log('File successfully written');
-    }
-});
-
-var res = notes.addNote();
-console.log(res);
-
-var addRes = notes.add(3, -9);
-console.log(addRes);
--- notes/notes.js
+++ notes/notes.js
@@ -6,9 +6,3 @@ module.exports.addNote = () => {
     console.log('addNote');
     return 'New note';
 };
-
-module.exports.add = (a, b) => {
-    console.log('add');
-    console.log('adding');
-    return a + b;
-}
